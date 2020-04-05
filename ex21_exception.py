57cc5119f2ffc426620f13cebf6e6e556d1ab627
Kimhongkyun <namu8si@gmail.com>
2020-04-05 22:55:20 +0900

    Update exception handling
    Several types have been added.

--- ex21_exception.py
+++ ex21_exception.py
@@ -85,3 +85,45 @@ try:
 except Exception as e:
     print("error:", e)
 
+print("=" * 40)
+
+x = int(input("a multiple of four: "))
+assert x % 4 == 0, 'It\'s not a multiple of 4.'
+print("x:", x)
+print("=" * 40)
+
+# Making Exception
+# Type1
+'''
+class NotThreeMultipleError(Exception):
+    def __init__(self):
+        super().__init__('It\'s not a multiple of 3.')
+
+def three_multiple():
+    try:
+        x = int(input("a multiple of three: "))
+        if x % 3 != 0:
+            raise NotThreeMultipleError
+        print(x)
+    except Exception as e:
+        print('e:', e)
+
+
+three_multiple()
+'''
+
+# Type2
+class NotThreeMultipleError(Exception):
+    pass
+
+def three_multiple2():
+    try:
+        x = int(input("a multiple of three: "))
+        if x % 3 != 0:
+            raise NotThreeMultipleError('It\'s not a multiple of 3.')
+        print(x)
+    except Exception as e:
+        print('e:', e)
+
+
+three_multiple2()
