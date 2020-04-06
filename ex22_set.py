4dddc405bc1ee30a78ab6110a2f1f4ba9561ed35
Kimhongkyun <namu8si@gmail.com>
2020-04-06 23:13:33 +0900

    Use a set
    Made it easy to handle a set.

---
+++ ex22_set.py
@@ -0,0 +1,13 @@
+colors = {"red", "green", "blue"}
+print(colors)
+
+e = set()
+print(e)
+
+if 'green' in colors:
+    print("True")
+
+a = set(range(5))
+b = set('apple')
+print("a: {}, b: {}".format(a, b))
+
