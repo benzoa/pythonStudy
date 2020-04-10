9a9897cbfe3b88afb107c077b55195ceb2a0d5a9
Kimhongkyun <namu8si@gmail.com>
2020-04-10 21:13:03 +0900

    Classify files and clean up them in directories
    It was time to organize the files.

--- pyqt5/ex2_checkbox.py
+++ pyqt5/ex2_checkbox.py
@@ -2,21 +2,12 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-form_class = uic.loadUiType("ex24_PyQtButton.ui")[0]
+form_class = uic.loadUiType("ui/checkbox.ui")[0]
 
 class WindowClass(QMainWindow, form_class):
     def __init__(self) :
         super().__init__()
         self.setupUi(self)
-
-        # push button signal
-        self.push_btn1.clicked.connect(self.push_btn1_func)
-        self.push_btn2.clicked.connect(self.push_btn2_func)
-
-        # radio button signal
-        self.radio_btn1.clicked.connect(self.radio_group_box_func)
-        self.radio_btn2.clicked.connect(self.radio_group_box_func)
-        self.radio_btn3.clicked.connect(self.radio_group_box_func)
 
         # group check box signal
         self.group_check_box1.stateChanged.connect(self.check_group_box_func)
@@ -27,18 +18,6 @@ class WindowClass(QMainWindow, form_class):
         self.check_box1.stateChanged.connect(self.check_box_func)
         self.check_box2.stateChanged.connect(self.check_box_func)
         self.check_box3.stateChanged.connect(self.check_box_func)
-
-
-    def push_btn1_func(self):
-        print("btn1 clicked")
-
-    def push_btn2_func(self):
-        print("btn2 clicked")
-
-    def radio_group_box_func(self):
-        if self.radio_btn1.isChecked(): print("radio1 checked")
-        elif self.radio_btn2.isChecked(): print("radio2 checked")
-        elif self.radio_btn3.isChecked(): print("radio3 checked")
     
     def check_group_box_func(self):
         if self.group_check_box1.isChecked(): print("group checkbox1 checked")
