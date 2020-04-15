774d17763b47841c2893215af55ef820dee64f04
Kimhongkyun <namu8si@gmail.com>
2020-04-15 22:16:59 +0900

    Update Date/Time Edit
    Added signals to invoke when values change

--- pyqt5/ex9_date_time_edit.py
+++ pyqt5/ex9_date_time_edit.py
@@ -14,16 +14,29 @@ class WindowClass(QMainWindow, form_class):
         self.dateTimeVar = QDateTime.currentDateTime()
         self.date_time_edit.setDateTime(self.dateTimeVar)
 
-        self.date_time_info.clicked.connect(self.date_time_info_disp)
-        self.date_time_set_date_time.clicked.connect(self.set_date_and_time)
-        self.date_time_set_date.clicked.connect(self.set_date)
-        self.date_time_set_time.clicked.connect(self.set_time)
-        self.date_time_change_format.clicked.connect(self.change_format)
-        self.date_time_print_range.clicked.connect(self.print_range)
-        self.date_time_plus_10day.clicked.connect(self.plus_10days)
-        self.date_time_minus_10days.clicked.connect(self.minus_10days)
+        self.date_time_edit.dateTimeChanged.connect(self.dte_date_time_changed)
+        self.date_time_edit.dateChanged.connect(self.dte_date_changed)
+        self.date_time_edit.timeChanged.connect(self.dte_time_changed)
 
-    def date_time_info_disp(self):
+        self.date_time_info.clicked.connect(self.dte_info_display)
+        self.date_time_set_date_time.clicked.connect(self.dte_set_date_time)
+        self.date_time_set_date.clicked.connect(self.dte_set_date)
+        self.date_time_set_time.clicked.connect(self.dte_set_time)
+        self.date_time_change_format.clicked.connect(self.dte_change_format)
+        self.date_time_print_range.clicked.connect(self.dte_print_range)
+        self.date_time_plus_10day.clicked.connect(self.dte_plus_10days)
+        self.date_time_minus_10days.clicked.connect(self.dte_minus_10days)
+
+    def dte_date_time_changed(self):
+        print("DateTime Changed: {}".format(self.date_time_edit.dateTime()))
+
+    def dte_date_changed(self):
+        print("Date Changed: {}".format(self.date_time_edit.date()))
+
+    def dte_time_changed(self):
+        print("Time Changed: {}".format(self.date_time_edit.time()))
+
+    def dte_info_display(self):
         self.disp_date_time = self.date_time_edit.dateTime()
         self.fmt = "yyyy-MM-dd hh:mm:ss"
         self.date_time_date_time_lb.setText(self.disp_date_time.toString(self.fmt))
@@ -36,35 +49,35 @@ class WindowClass(QMainWindow, form_class):
         self.fmt = "AP hh:mm:ss"
         self.date_time_time_lb.setText( self.disp_time.toString(self.fmt))
 
-    def set_date_and_time(self):
+    def dte_set_date_time(self):
         self.date_and_time = self.date_time_date_time_le.text()
         self.le_date_time_var = QDateTime.fromString(self.date_and_time, "yyyy-MM-dd AP hh:mm:ss")
         self.date_time_edit.setDateTime(self.le_date_time_var)
 
-    def set_date(self):
+    def dte_set_date(self):
         self.le_date = self.date_time_date_le.text()
         self.le_date_var = QDate.fromString(self.le_date, "yyyy-MM-dd")
         self.date_time_edit.setDate(self.le_date_var)
 
-    def set_time(self):
+    def dte_set_time(self):
         self.le_time = self.date_time_time_le.text()
         self.le_time_var = QTime.fromString(self.le_time, "AP hh:mm:ss")
         self.date_time_edit.setTime(self.le_time_var)
 
-    def change_format(self):
+    def dte_change_format(self):
         self.new_format = self.date_time_disp_format_le.text()
         self.date_time_edit.setDisplayFormat(self.new_format)
 
-    def print_range(self):
+    def dte_print_range(self):
         print("maximumDateTime: {}".format(self.date_time_edit.maximumDateTime()))
         print("minimumDateTime: {}".format(self.date_time_edit.minimumDateTime()))
 
-    def plus_10days(self):
+    def dte_plus_10days(self):
         self.cur_maximum_date_time = self.date_time_edit.maximumDateTime()
         self.cur_maximum_date_time = self.cur_maximum_date_time.addDays(10)
         self.date_time_edit.setMaximumDateTime(self.cur_maximum_date_time)
 
-    def minus_10days(self):
+    def dte_minus_10days(self):
         self.cur_minimum_date_time = self.date_time_edit.minimumDateTime()
         self.cur_minimum_date_time = self.cur_minimum_date_time.addDays(-10)
         self.date_time_edit.setMinimumDateTime(self.cur_minimum_date_time)
