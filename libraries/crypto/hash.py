8d7fc30d88ae83c79bad8b2b5fef4e170ceac9dd
Kimhongkyun <namu8si@gmail.com>
2020-05-17 23:22:09 +0900

    Rename hash_md5_sha256.py -> libraries/crypto/hash.py
    Added how to use the built-in hash library.

--- libraries/crypto/hash.py
+++ libraries/crypto/hash.py
@@ -1,3 +1,4 @@
+# https://www.dlitz.net/software/pycrypto/
 # pip install pycryto
 
 from Crypto.Hash import MD5, SHA512
@@ -17,3 +18,23 @@ print("hash_md5.hexdigest():", hash_md5.hexdigest())
 
 hash_md5.update(b'egg')
 print("hash_md5.hexdigest():", hash_md5.hexdigest())
+
+import hashlib
+
+string = 'a'
+encoded_string = string.encode()
+print('encoded_string:', encoded_string)
+
+hexdigest = hashlib.sha256(encoded_string).hexdigest()
+print('type1 a:', hexdigest)
+
+sha256 = hashlib.new('sha256')
+sha256.update(encoded_string)
+print('tyep2 a:', sha256.hexdigest())
+
+string2 = 'a.'
+encoded_string2 = string2.encode()
+print('a.:', hashlib.sha256(encoded_string2).hexdigest())
+
+hash_md5 = hashlib.md5(b'hamegg')
+print('hash_md5.hexdigest():', hash_md5.hexdigest())
