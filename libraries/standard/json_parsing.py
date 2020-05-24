cc4a3a8bcb71940ccafae5689ca15def85000c8a
Kimhongkyun <namu8si@gmail.com>
2020-05-24 23:00:57 +0900

    Use the standard library JSON
    tried simple encoding and decoding using JSON.

---
+++ libraries/standard/json_parsing.py
@@ -0,0 +1,33 @@
+import json
+
+data = [{
+    'id':123,
+    'entities': {
+    'url': 'python.org',
+    'hashtags': ['#python', '#pythonkor']}}]
+
+print("dumps(data):", json.dumps(data, indent=2))
+
+json_str = '["ham", 1.0, {"a":false, "b":null}]'
+print("loads(json_str):", json.loads(json_str))
+
+from decimal import Decimal
+print("loads():", json.loads(json_str, parse_float=Decimal))
+
+data = {
+    "no": 5,
+    "code": ("jas", 1, 19),
+    "src": "be quick to listen, slow to speak, slow to anger"
+}
+
+
+filename = "test.json"
+with open(filename, "w") as fp:
+    fp.write(json.dumps(data))
+
+
+with open(filename, "r") as fp:
+    r = json.load(fp)
+    print("no:", r["no"])
+    print("code:", r["code"])
+    print("src:", r["src"])
