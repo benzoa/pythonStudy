5f677035c48831dc5c87aa0e72192acbd8be3127
Kimhongkyun <namu8si@gmail.com>
2020-06-24 22:28:24 +0900

    Use logger to save or output log messages

--- libraries/standard/lib_logger.py
+++ libraries/standard/lib_logger.py
@@ -1,5 +1,6 @@
 import logging as log
 import mylib
+import logging.handlers
 
 
 def simple_log():
@@ -22,7 +23,30 @@ def formatted_log():
     log.info('Finished')
 
 
+def use_logger():
+    logger = log.getLogger('mylogger')
+    logger.setLevel(log.DEBUG)
+    
+    fmt = '[%(levelname)s][%(filename)s:%(lineno)d][%(asctime)s] > %(message)s'
+    formatter = log.Formatter(fmt=fmt, datefmt='%y%m%d %H:%M:%S')
+
+    filename = './example.log'
+    
+    file_max_byte = 1024 * 1024 * 10 # 10MB
+    file_handler = logging.handlers.RotatingFileHandler(filename, maxBytes=file_max_byte, backupCount=20)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    stream_handler = log.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    
+    logger.debug('This message should appear on the console')
+    logger.info('So should this')
+    logger.warning('And this, too')  
+
+
 if __name__ == '__main__':
     # simple_log()
-    
-    formatted_log()
+    # formatted_log()
+    use_logger()
