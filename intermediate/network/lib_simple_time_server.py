b38f8d022085b03ed6bc3d60dc262ff3a3fea236
Kimhongkyun <namu8si@gmail.com>
2020-06-28 23:32:02 +0900

    Update the wrong description on a simple server

--- intermediate/network/lib_simple_time_server.py
+++ intermediate/network/lib_simple_time_server.py
@@ -1,14 +1,13 @@
 import socket
+import time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 addr = ('', 50000)
 sock.bind(addr)
 sock.listen(5)
-client_sock, addr = sock.accept()
-client_sock.send("message".encode())
 
 while True:
-    client, addr = socket.accept()
+    client, addr = sock.accept()
     print("Connected:", addr)
     client.send(time.ctime(time.time()).encode())
     client.close()
