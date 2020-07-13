bbc1b85455d86d4776cc45a37d6a07abd521eaa8
Kimhongkyun <namu8si@gmail.com>
2020-07-13 22:10:48 +0900

    Check whether the method works normally through unittest

---
+++ libraries/unittest/unittest_testcase.py
@@ -0,0 +1,27 @@
+import unittest
+
+
+def sum(a, b):
+    return a + b
+
+class ModuleTest(unittest.TestCase):
+    def setUp(self):
+        self.bag = [True, True]
+    
+    def test_true(self):
+        for element in self.bag:
+            self.assertTrue(element)
+    
+    def tearDown(self):
+        del self.bag
+    
+    def testSum1(self):
+        self.assertEqual(sum(1, 2), 3)
+    
+    def testSum2(self):
+        self.assertEqual(sum(1, -1), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
