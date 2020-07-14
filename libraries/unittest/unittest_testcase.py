f561b9fcd35da21fa5b0055757335cf62af903a3
Kimhongkyun <namu8si@gmail.com>
2020-07-15 05:22:21 +0900

    Add setUpClass ans tearDownClass

--- libraries/unittest/unittest_testcase.py
+++ libraries/unittest/unittest_testcase.py
@@ -5,15 +5,23 @@ def sum(a, b):
     return a + b
 
 class ModuleTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')   
+    
     def setUp(self):
         self.bag = [True, True]
-    
+
+    def tearDown(self):
+        del self.bag
+
     def test_true(self):
         for element in self.bag:
             self.assertTrue(element)
-    
-    def tearDown(self):
-        del self.bag
     
     def testSum1(self):
         self.assertEqual(sum(1, 2), 3)
@@ -24,4 +32,3 @@ class ModuleTest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
