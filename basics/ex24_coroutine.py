e57356ddfb1e7d1c0e787039372c7f53267803ba
Kimhongkyun <namu8si@gmail.com>
2020-07-21 22:16:34 +0900

    Update coroutine.py

--- basics/ex24_coroutine.py
+++ basics/ex24_coroutine.py
@@ -26,3 +26,22 @@ print(next(co2))
 print(co2.send(1))
 print(co2.send(2))
 print(co2.send(3))
+
+# ------------------------------
+def sum_coroutine():
+    try:
+        total = 0
+        while True:
+            x = (yield)
+            total += x
+    except RuntimeError as e:
+        print(e)
+        yield total
+ 
+co3 = sum_coroutine()
+next(co3)
+ 
+for i in range(20):
+    co3.send(i)
+
+print(co3.throw(RuntimeError, 'Break coroutine with exception'))
