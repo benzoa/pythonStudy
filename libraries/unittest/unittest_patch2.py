fa3c09613e1d5e917715ef9877b4b178d7ea5df7
Kimhongkyun <namu8si@gmail.com>
2020-07-26 23:16:55 +0900

    Second example of using patch in unittest

---
+++ libraries/unittest/unittest_patch2.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+import user_manager
+
+
+class TestUserManger(TestCase):
+    @patch("requests.post")
+    def test_create_user(self, mock_post):
+        response = mock_post.return_value
+        response.status_code = 201
+        response.json.return_value = {"id": 99}
+
+        user = user_manager.create_user(
+            {"name": "Test User", "email": "user@test.com",}
+        )
+
+        self.assertEqual(user["id"], 99)
+        mock_post.assert_called_once_with(
+            "https://jsonplaceholder.typicode.com/users",
+            data={"name": "Test User", "email": "user@test.com",},
+        )
+		
