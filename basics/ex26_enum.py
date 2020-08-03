19cef678fab7cbf85e7f89858714077abc0a0bac
Kimhongkyun <namu8si@gmail.com>
2020-08-03 23:15:01 +0900

    Modify enum usage

--- basics/ex26_enum.py
+++ basics/ex26_enum.py
@@ -8,3 +8,25 @@ class Skill(Enum):
 
 print(Skill.HTML.value, Skill.HTML.name)
 print(Skill.Python.value, Skill.Python.name)
+
+Skill = Enum("Skill", "HTML CSS JS")
+print(list(Skill))
+
+class StrEnum(str, Enum):
+    def _generate_next_value_(name, start, count, last_values):
+        return name
+
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class MySkill(StrEnum):
+    HTML = auto()
+    CSS = auto()
+    JS = auto()
+
+print(MySkill.HTML == "HTML")
+print(isinstance(MySkill.HTML, str))
