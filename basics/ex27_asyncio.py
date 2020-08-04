18d46026b47f7e445cde38fbc72b8b6a5cb150b7
Kimhongkyun <namu8si@gmail.com>
2020-08-05 07:06:04 +0900

    Update asyncio usage

--- basics/ex27_asyncio.py
+++ basics/ex27_asyncio.py
@@ -1,6 +1,8 @@
 import asyncio
+import time
 
 async def add(a, b):
+    await asyncio.sleep(1.0)
     return a + b
 
 async def hello(a, b):
@@ -8,6 +10,28 @@ async def hello(a, b):
     ret = await add(a, b)
     print("{} + {} = {}".format(a, b, ret))
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(hello(10, 20))
-loop.close()
+async def coroutine_1():
+    print("Start coroutine 1")
+    await asyncio.sleep(3.0)
+    print("Resume coroutine 1")
+
+async def coroutine_2():
+    print("Start coroutine 2")
+    await asyncio.sleep(2.0)
+    print("Resume coroutine 2")
+
+async def coroutine_3():
+    print("Start coroutine 3")
+    loop = asyncio.get_event_loop()
+    # to make as subroutine act like a coroutine with threading
+    loop.run_in_executor(None, time.sleep, 1)
+    print("Resume coroutine 3")
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+
+    start = time.time()
+    # Specify scheduling order by asyncio.gather
+    loop.run_until_complete(asyncio.gather(coroutine_1(), coroutine_2(), coroutine_3(), hello(10, 20)))
+    print(f"taken: {time.time() - start}")
+    loop.close()
