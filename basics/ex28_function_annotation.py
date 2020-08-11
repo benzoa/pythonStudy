50e08e97f3f233eed4c0c97d1837e0693f347a34
Kimhongkyun <namu8si@gmail.com>
2020-08-11 23:53:42 +0900

    Use annotations for variables in the class

--- basics/ex28_function_annotation.py
+++ basics/ex28_function_annotation.py
@@ -5,4 +5,28 @@ def func(arg1:str, arg2: 1+2, arg3: 'this is annottion') -> bool:
     return True
 
 ret = func('test1', 3, 'this is test')
-print(f'result = {ret}')
+print(f'result = {ret}\n')
+
+class AnnotationClassExample:
+    first_param: str
+    second_param: int
+
+    def set_first_param(self, value: str) -> None:
+        self.first_param = value
+
+    def set_second_param(self, value: int) -> bool:
+        if type(value) == int:
+            self.second_param = value
+            return True
+        else:
+            self.second_param = 0
+            return False
+
+def main():
+    new_class = AnnotationClassExample()
+    print(new_class.__annotations__)
+    print(new_class.set_first_param.__annotations__)
+    print(new_class.set_second_param.__annotations__)
+
+if __name__ == '__main__':
+    main()
