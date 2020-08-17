59db0fbfab038f9aec8b66b685fd97c71363b938
Kimhongkyun <namu8si@gmail.com>
2020-08-17 23:15:31 +0900

    Draw a graph of the highest temperature

--- simple_project/data_analysis/seoul_temperature/README.md
+++ simple_project/data_analysis/seoul_temperature/README.md
@@ -1,1 +1,2 @@
 # The project was created to analyze temperature data in Seoul from June 1, 2018 to July 31, 2020.
+***
--- simple_project/data_analysis/seoul_temperature/src/main.py
+++ simple_project/data_analysis/seoul_temperature/src/main.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 from enum import Enum
 
 Idx = Enum("Idx", "DATE LOC MEAN_TEMP MIN_TEMP MAX_TEMP", start=0)
@@ -12,6 +13,8 @@ low_temp_max_date = ''
 low_temp_min_val = 0
 low_temp_min_date = ''
 
+result = []
+
 with open("./resource/seoul.csv", "r", encoding='cp949') as f:
     data = csv.reader(f, delimiter=",")
     header = next(data)
@@ -20,6 +23,7 @@ with open("./resource/seoul.csv", "r", encoding='cp949') as f:
     for row in data:
         if row[Idx.MAX_TEMP.value] != '':
             row[Idx.MAX_TEMP.value] = float(row[Idx.MAX_TEMP.value])
+            result.append(row[Idx.MAX_TEMP.value])
         
             if row[Idx.MAX_TEMP.value] > high_temp_max_val:
                 high_temp_max_val = row[Idx.MAX_TEMP.value]
@@ -42,7 +46,8 @@ with open("./resource/seoul.csv", "r", encoding='cp949') as f:
             low_temp_min_date = row[Idx.DATE.value]
 
         # print(row)
-
+    plt.plot(result, 'r')
+    plt.show()
     print(f"high_temp_max_val : {high_temp_max_val}, date : {high_temp_max_date}")
     print(f"high_temp_min_val : {high_temp_min_val}, date : {high_temp_min_date}")
     print(f"low_temp_max_val : {low_temp_max_val}, date : {low_temp_max_date}")
