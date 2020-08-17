d21c5dbfe98a49fdee6dfc7b0f8694a3ca6285da
Kimhongkyun <namu8si@gmail.com>
2020-08-18 07:12:02 +0900

    Graph the highest and lowest temperatures

--- simple_project/data_analysis/seoul_temperature/src/main.py
+++ simple_project/data_analysis/seoul_temperature/src/main.py
@@ -12,8 +12,8 @@ low_temp_max_val = 0
 low_temp_max_date = ''
 low_temp_min_val = 0
 low_temp_min_date = ''
-
-result = []
+high = []
+low = []
 
 with open("./resource/seoul.csv", "r", encoding='cp949') as f:
     data = csv.reader(f, delimiter=",")
@@ -23,7 +23,7 @@ with open("./resource/seoul.csv", "r", encoding='cp949') as f:
     for row in data:
         if row[Idx.MAX_TEMP.value] != '':
             row[Idx.MAX_TEMP.value] = float(row[Idx.MAX_TEMP.value])
-            result.append(row[Idx.MAX_TEMP.value])
+            high.append(row[Idx.MAX_TEMP.value])
         
             if row[Idx.MAX_TEMP.value] > high_temp_max_val:
                 high_temp_max_val = row[Idx.MAX_TEMP.value]
@@ -37,6 +37,7 @@ with open("./resource/seoul.csv", "r", encoding='cp949') as f:
             continue
         
         row[Idx.MIN_TEMP.value] = float(row[Idx.MIN_TEMP.value])
+        low.append(row[Idx.MIN_TEMP.value])
 
         if row[Idx.MIN_TEMP.value] > low_temp_max_val:
             low_temp_max_val = row[Idx.MIN_TEMP.value]
@@ -46,7 +47,14 @@ with open("./resource/seoul.csv", "r", encoding='cp949') as f:
             low_temp_min_date = row[Idx.DATE.value]
 
         # print(row)
-    plt.plot(result, 'r')
+    
+
+    plt.figure(figsize = (15, 3))
+    plt.rc('font', family = 'Malgun Gothic')
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.title('Temperature data in Seoul from June 1, 2018 to July 31, 2020')
+    plt.plot(high, 'red')
+    plt.plot(low, 'skyblue')
     plt.show()
     print(f"high_temp_max_val : {high_temp_max_val}, date : {high_temp_max_date}")
     print(f"high_temp_min_val : {high_temp_min_val}, date : {high_temp_min_date}")
