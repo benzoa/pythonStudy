954cbf0a7e5a6557f23b7cebdc9f0788f9e059c6
Kimhongkyun <namu8si@gmail.com>
2020-10-21 23:37:52 +0900

    Correct pyqt example

--- pyqt5/examples/ex1_controlling_text_of_the_line_edit/controlling_text_of_the_line_edit.py
+++ pyqt5/examples/ex1_controlling_text_of_the_line_edit/controlling_text_of_the_line_edit.py
@@ -1,24 +1,23 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+from PyQt5.QtWidgets import QWidget, QApplication, QLineEdit
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIntValidator, QDoubleValidator
 from PyQt5 import uic
 
-form_class = uic.loadUiType("controling_text_of_the_line_edit.ui")[0]
+form_class = uic.loadUiType("controlling_text_of_the_line_edit.ui")[0]
 
-class WindowClass(QMainWindow, form_class) :
-    def __init__(self) :
+
+class WindowClass(QWidget, form_class):
+    def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.echo_le.setFocus()
-        
+
         self.echo_cb.activated.connect(self.echo_changed)
         self.validator_cb.activated.connect(self.validator_changed)
         self.alignment_cb.activated.connect(self.alignment_changed)
         self.input_mask_cb.activated.connect(self.input_mask_changed)
         self.access_cb.activated.connect(self.access_changed)
-
 
     def echo_changed(self, index):
         if index == 0:
@@ -29,7 +28,6 @@ class WindowClass(QMainWindow, form_class) :
             self.echo_le.setEchoMode(QLineEdit.Password)
         elif index == 3:
             self.echo_le.setEchoMode(QLineEdit.PasswordEchoOnEdit)
-
 
     def validator_changed(self, index):
         if index == 0:
@@ -43,7 +41,6 @@ class WindowClass(QMainWindow, form_class) :
 
         self.validator_le.clear()
 
-
     def alignment_changed(self, index):
         if index == 0:
             self.alignment_le.setAlignment(Qt.AlignLeft)
@@ -51,7 +48,6 @@ class WindowClass(QMainWindow, form_class) :
             self.alignment_le.setAlignment(Qt.AlignCenter)
         elif index == 2:
             self.alignment_le.setAlignment(Qt.AlignRight)
-
 
     def input_mask_changed(self, index):
         if index == 0:
@@ -65,7 +61,6 @@ class WindowClass(QMainWindow, form_class) :
         elif index == 3:
             self.input_mask_le.setInputMask('>AAAAA-AAAAA-AAAAA-AAAAA;#')
 
-
     def access_changed(self, index):
         if index == 0:
             self.access_le.setReadOnly(False)
@@ -73,7 +68,7 @@ class WindowClass(QMainWindow, form_class) :
             self.access_le.setReadOnly(True)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWindow = WindowClass()
     myWindow.show()
